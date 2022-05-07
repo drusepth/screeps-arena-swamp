108634f669b33c648ed7811c01d361e822324860
Andrew Brown <drusepth@gmail.com>
2022-05-06 19:14:04 -0700

    performant circular point generator

---
+++ helpers/geometry.mjs
@@ -0,0 +1,21 @@
+export function all_points_in_circle(xCenter, yCenter, radius) {
+    var points_in_circle = [];
+
+    for (var x = xCenter - radius ; x <= xCenter; x++) {
+        for (var y = yCenter - radius ; y <= yCenter; y++) {
+            // Since this is a circle, we can speed this up 4x (and avoid sqrt) by taking symmetric
+            // points from a single quadrant.
+            if ((x - xCenter) * (x - xCenter) + (y - yCenter) * (y - yCenter) <= r * r) {
+                xSym = xCenter - (x - xCenter);
+                ySym = yCenter - (y - yCenter);
+
+                points_in_circle.push({ x: x, y: y});
+                points_in_circle.push({ x: x, y: ySym});
+                points_in_circle.push({ x: xSym, y: y});
+                points_in_circle.push({ x: xSym, y: ySym});
+            }
+        }
+    }
+
+    return points_in_circle;
+}
